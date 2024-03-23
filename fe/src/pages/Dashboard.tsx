4c0b95ee6247e9bd969183832c19999db2bb9664
ssk-12 <ullegadda.srikanta@gmail.com>
2024-03-24 01:38:59 +0530

    fixed distance check condition

--- fe/src/pages/Dashboard.tsx
+++ fe/src/pages/Dashboard.tsx
@@ -71,18 +71,19 @@ const Dashboard: React.FC = () => {
       
       
 
-    function calculateDistance(lat1, lon1, lat2, lon2) {
-        const R = 6371; 
-        const dLat = (lat2 - lat1) * (Math.PI / 180);  
+      function calculateDistance(lat1, lon1, lat2, lon2) {
+        const R = 6371000; // Earth's radius in meters
+        const dLat = (lat2 - lat1) * (Math.PI / 180);
         const dLon = (lon2 - lon1) * (Math.PI / 180);
         const a = 
           Math.sin(dLat / 2) * Math.sin(dLat / 2) +
           Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) * 
           Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-        const distance = R * c; // Distance in kilometers
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        const distance = R * c; // Distance in meters
         return distance;
-      }
+    }
+    
       
 
     function deg2rad(deg: number): number {
